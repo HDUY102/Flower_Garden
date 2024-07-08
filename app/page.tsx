bcee59cba99fd32885048ef3b01e4730b5925b18
HDUY <leduy020702@gmail.com>
2024-07-08 09:47:36 +0700

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Header from "./components/Header";
-import Slider from "./components/Slider";
 import HomeSection from "./components/HomeSection";
 import Footer from "./components/Footer";
 import About from "./components/About";
