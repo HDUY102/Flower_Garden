e668f23ae2e5ed22f05c02ee8ded19b65911bb5b
HDUY <leduy020702@gmail.com>
2024-07-11 11:18:08 +0700

    Update code and css Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,10 +11,11 @@ const Header = () => {
   const handleNav = () => {
     setNav(!nav);
   };
+  const handleCloseNav = () => {
+    setNav(false);
+  };
   return (
-    // <div className="fixed left-0 w-full z-10 ease-in duration-300 ">
     <div>
-      {/* <header className="max-w-[1240px] m-auto flex justify-between items-center p-3 text-black shadow-lg z-1000"> */}
       <header className={style.header}>
         <div>
           <a href={"/"} className="font-bold text-2xl">
@@ -23,18 +24,11 @@ const Header = () => {
             </h1>
           </a>
         </div>
-        {/* <ul className="gap-x-32 items-center font-extralbold p-1 hidden sm:flex">
-          <li className="hover:text-pink-400 p-4">Home</li>
-          <li className="hover:text-pink-400 p-4">About</li>
-          <li className="hover:text-pink-400 p-4">Product</li>
-          <li className="hover:text-pink-400 p-4">Review</li>
-          <li className="hover:text-pink-400 p-4">Contact Us</li>
-        </ul> */}
-        <nav className={style.navbar}>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#product">Product</a>
-          <a href="#review">Review</a>
+        <nav className={`${style.navbar} ${nav ? style.active : ''}`}>
+          <a href="#home" onClick={handleCloseNav}>Home</a>
+          <a href="#about" onClick={handleCloseNav}>About</a>
+          <a href="#product" onClick={handleCloseNav}>Product</a>
+          <a href="#review" onClick={handleCloseNav}>Review</a>
         </nav>
         <div className={style.icons}>
           <a href="#"><FaCartShopping/></a>
@@ -42,11 +36,15 @@ const Header = () => {
           <a href="#"><FaCircleUser/></a>
         </div>
         {/* mobile button */}
-        <div onClick={handleNav} className="block sm:hidden z-10 ">
-          {nav ? <AiOutlineClose size={20} className="text-white"/> : <AiOutlineMenu size={20} className=""/>}
+        <div onClick={handleNav} className={`${style.toggle} block sm:hidden z-10`}>
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
+        {/* <div onClick={handleNav} className="block sm:hidden z-10">
+        <div onClick={handleNav} className={style.toggle}>
+          {nav ? <AiOutlineClose size={20} className="text-white"/> : <AiOutlineMenu size={20} className=""/>}
+        </div> */}
         {/* mobile menu */}
-        <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen  bg-black text-center ease-in duration-300':
+        {/* <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen  bg-black text-center ease-in duration-300':
             'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
         }>
           <ul className="gap-x-20 items-center font-extralbold p-2 text-white">
@@ -56,7 +54,7 @@ const Header = () => {
             <li className="hover:text-pink-400">Review</li>
             <li className="hover:text-pink-400">Contact Us</li>
           </ul>
-        </div>
+        </div> */}
       </header>
     </div>
   );
