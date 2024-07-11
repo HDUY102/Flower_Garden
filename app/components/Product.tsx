770e7fe7208bf7ddcfa02b29a7bbd11f72d35379
HDUY <leduy020702@gmail.com>
2024-07-11 17:43:33 +0700

    Update css and code

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -52,6 +52,66 @@ const Product = () => {
                     <div className={style.price}>$12.99 <span>$15.99</span></div>
                 </div>
             </div>
+            <div className={style.box}>
+                <p className={style.discount}>-20%</p>
+                <div className={style.image}>
+                    <img src="/flower1.png" alt="" />
+                    <div className={style.button}>
+                        <a href=""><FaHeart className={style.icon}/></a>
+                        <a className={style.addCart}>Add To Cart</a>
+                        <a href=""><FaShare className={style.icon}/></a>
+                    </div>
+                </div>
+                <div className={style.content}>
+                    <h3>Flower Pot</h3>
+                    <div className={style.price}>$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+            <div className={style.box}>
+                <p className={style.discount}>-20%</p>
+                <div className={style.image}>
+                    <img src="/flower1.png" alt="" />
+                    <div className={style.button}>
+                        <a href=""><FaHeart className={style.icon}/></a>
+                        <a className={style.addCart}>Add To Cart</a>
+                        <a href=""><FaShare className={style.icon}/></a>
+                    </div>
+                </div>
+                <div className={style.content}>
+                    <h3>Flower Pot</h3>
+                    <div className={style.price}>$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+            <div className={style.box}>
+                <p className={style.discount}>-20%</p>
+                <div className={style.image}>
+                    <img src="/flower1.png" alt="" />
+                    <div className={style.button}>
+                        <a href=""><FaHeart className={style.icon}/></a>
+                        <a className={style.addCart}>Add To Cart</a>
+                        <a href=""><FaShare className={style.icon}/></a>
+                    </div>
+                </div>
+                <div className={style.content}>
+                    <h3>Flower Pot</h3>
+                    <div className={style.price}>$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+            <div className={style.box}>
+                <p className={style.discount}>-20%</p>
+                <div className={style.image}>
+                    <img src="/flower1.png" alt="" />
+                    <div className={style.button}>
+                        <a href=""><FaHeart className={style.icon}/></a>
+                        <a className={style.addCart}>Add To Cart</a>
+                        <a href=""><FaShare className={style.icon}/></a>
+                    </div>
+                </div>
+                <div className={style.content}>
+                    <h3>Flower Pot</h3>
+                    <div className={style.price}>$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
         </div>
     </section>
   )
