153c7d7196593d5320d815c07990a1ed078a1f54
HDUY <leduy020702@gmail.com>
2024-07-15 08:47:31 +0700

    Push code main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,18 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Product from "./components/Product";
 import Icons from "./components/Icons";
+import Review from "./components/Review";
 
 export default function Home() {
   return (
-    // <div className='custom-img h-screen bg-center bg-fixed'>
     <div>
       <Header/>
       <HomeSection/>
       <About/>
       <Icons/>
       <Product/>
+      <Review/>
       <Footer/>
-      {/* <Slider/> */}
     </div>
   );
 }
